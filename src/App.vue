78598dfabb72f9758a17603f5b63fd3f9408cf1c
yoshin04 <taigaby0411@gmail.com>
2024-01-17 15:50:11 +0900

    wip

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,7 @@
 
     <div>
       <h2>イベントのフォーム</h2>
-      <label for="タイトル">タイトル</label>
-      <input type="text" id="title" v-model="eventData.title" />
-      <p>タイトル: {{eventData.title}}</p>
+      <EventTitle v-model="eventData.title" />
 
       <label for="maxNumber">最大人数</label>
       <input type="number" id="maxNumber" v-model.number="eventData.maxNumber" />
@@ -32,6 +30,32 @@
       <input type="text" id="host" v-model.trim="eventData.host" />
       <p>ホスト: {{eventData.host}}</p>
 
+      <textarea id="detail" v-model="eventData.detail"></textarea>
+      <pre>詳細: {{eventData.detail}}</pre>
+
+      <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate" />
+      <label for="isPrivate">非公開</label>
+      <p>非公開: {{eventData.isPrivate}}</p>
+      <p>参加条件</p>
+      <label for="10">10代</label>
+      <input type="checkbox" id="10" value="10代" v-model="eventData.target">
+      <label for="20">20代</label>
+      <input type="checkbox" id="20" value="20代" v-model="eventData.target">
+       <label for="30">30代</label>
+      <input type="checkbox" id="30" value="30代" v-model="eventData.target">
+      <p>参加条件: {{eventData.target}}</p>
+
+      <p>参加費</p>
+      <input type="radio" id="free" value="無料" v-model="eventData.fee">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.fee">
+      <label for="paid">有料</label>
+
+      <p>開催場所</p>
+      <select v-model="eventData.location">
+        <option v-for="location in locations" :key="location">{{location}}</option>
+      </select>
+      <p>{{eventData.location}}</p>
     </div>
   </div>
 </template>
@@ -40,16 +64,23 @@
   import LikeHeader from './components/LikeHeader.vue'
   import HomeView from './components/HomeView.vue'
   import AboutView from './components/AboutView.vue'
+  import EventTitle from './components/EventTitle.vue'
 
   export default {
     data: function() {
       return {
         number: 10,
         currentComponent: 'HomeView',
+        locations: ['東京', '神奈川'],
         eventData: {
           title: 'タイトル',
           maxNumber: 0,
           host: 'ホスト',
+          detail: '',
+          isPrivate: false,
+          target: [],
+          fee: '無料',
+          location: '東京'
         }
       }
     },
@@ -57,6 +88,7 @@
       LikeHeader,
       HomeView,
       AboutView,
+      EventTitle,
     },
     methods: {
       incrementCount() {
---
+++ src/components/EventTitle.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <label for="タイトル">タイトル</label>
+      <input type="text" id="title" :value="value" @input="$emit('input', $event.target.value)" />
+      <p>タイトル: {{value}}</p>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["value"]
+  }
+</script>
